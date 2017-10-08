bc688a298d0d0646b2e8b4d193847280ec1cb322
sashotim <aleksandar.timanov@rwth-aachen.de>
2017-10-08 16:37:48 +0200

    first 2 JS Exercises

---
+++ jsExercise/firstExercise/JSfirstExercise.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What i your first name?");
+var lastName = prompt("What i your last name?");
+var userAge = prompt("What i your age?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("Your age is " + userAge);
---
+++ jsExercise/secondExercise(AgeCalc)/calc.js
@@ -0,0 +1,4 @@
+var age = prompt("What is your age?");
+var days = age * 365.25;
+
+alert("Your age in days is roughly " + days);
